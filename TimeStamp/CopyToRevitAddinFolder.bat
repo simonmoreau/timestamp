cd6834be72457b74c67d180749589d172a5f836b
Simon Moreau <simon@bim42.com>
2018-05-27 16:03:16 +0200

    Add bat file to deplace files for testing

---
+++ TimeStamp/CopyToRevitAddinFolder.bat
@@ -0,0 +1,6 @@
+copy "TimeStamp.addin" "%AppData%\Autodesk\REVIT\Addins\2019"
+copy "bin\debug\TimeStamp.dll" "%AppData%\Autodesk\REVIT\Addins\2019"
+copy "Resources\TimeStampHelp.html" "%AppData%\Autodesk\REVIT\Addins\2019"
+mkdir "%AppData%\Autodesk\REVIT\Addins\2019\TimeStampHelp_Files"
+copy "Resources\TimeStampHelp_Files" "%AppData%\Autodesk\REVIT\Addins\2019\TimeStampHelp_Files"
+pause
